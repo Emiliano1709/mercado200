168fe1420c040de22b4b3471e22228c47b889bbf
Emiliano1709 <emiliano.lopez1417@gmail.com>
2025-05-26 17:15:27 -0600

    V.2.0.0

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mercado200
-Usando agentes de IA, impulsados por Open AI, se espera conseguir una herramienta de apoyo a la investigación de mercados específicos y obtener un panorama parcial de la posición de una empresa o negocio
+Desarrollado por SERGIO EMILIANO LOPEZ BAUTISTA [25 04 2025] 
+Colaboración con:
+
+NOTA IMPORTANTE:En caso de hacer una modificación, colocarla en una nueva rama. 
+Si se logra una nueva versión estable, coloca tu nombre en el apartado de COLABORACIÓN y notificalo en este README.md con los cambios agregados respecto a la versión anterior.
+
+= V.2.0.0 = 
+    Versión beta 
+Un sólo agente realiza un trabajo general de investigación con un prompt no optimizado colocado directamente como un f-string
---
+++ mercado200.py
@@ -0,0 +1,63 @@
+###################################################
+#              Generador de leads                 #
+# V.2.0.0 //25 04 2025//                          #
+# Desplegado con streamlit                        #
+# Agentes impulsados con OpenAI                   #
+# Desarrollador: Sergio Emiliano López Bautista   #
+###################################################
+
+
+# ------------------------- Requerimentos y librerías -------------------------------
+import io
+import os
+import time
+import codecs
+import streamlit as st
+from dotenv import load_dotenv, find_dotenv
+from openai import OpenAI
+
+# --------------------------- Seteadores ----------------------------------------------
+st.set_page_config(page_title="Estudio de mercado", layout="wide")
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path, override=True)
+client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
+#client = OpenAI(api_key = st.secrets["OPENAI_API_KEY"])
+
+st.title("Estudio de mercado")
+
+# --------------------------- Funciones -----------------------------------------------
+def agente(tema):
+    try:
+        agente = client.responses.create(
+        model= "gpt-4.1",
+        input= f"Necesito un estudio de mercado acerca de {tema}"
+        )
+        return agente.output_text
+    except Exception as e:
+        st.error(f"Error: {str(e)}")
+        return None
+
+def maquina_de_escribir(respuesta):
+    for word in respuesta.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+def instrucciones():
+    with codecs.open("instrucciones.txt", "r", encoding="utf-8") as f:
+        fi = f.read()
+    file = fi.split('\n')
+    for linea in file:
+        st.markdown(linea)
+
+# -------------------------------- Interfaz (MAIN)-------------------------------------
+
+st.header("Dale, mano")
+tema = st.text_input("De qué tema buscas información")
+
+if st.button("investigar"):
+    if tema:
+        with st.spinner("investigando..."):
+            st.markdown("### Vista previa de la información")
+            st.markdown(agente(tema))
+            #st.write_stream(maquina_de_escribir(agente(tema)))
