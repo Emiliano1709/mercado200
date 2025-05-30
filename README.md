f8c67a581d9c65599bacf99d9fc23321ab2d73d0
Emiliano1709 <emiliano.lopez1417@gmail.com>
2025-05-30 17:55:51 -0600

    V.2.1.2

--- README.md
+++ README.md
@@ -8,3 +8,36 @@ Si se logra una nueva versión estable, coloca tu nombre en el apartado de COLAB
 = V.2.0.0 = 
     Versión beta 
 Un sólo agente realiza un trabajo general de investigación con un prompt no optimizado colocado directamente como un f-string
+
+= V.2.0.3 =
+    Prototipo de interfaz
+Los pequeños cambios agregados a esta versión involucran:
+    - Instrucciones cortas para introducir al usuario al funcionamiento general de la herramienta
+    - La animación de maquina de escribir al despliegue de la vista previa de la información
+    - Validación a la entradad de datos
+*Esta versión no se incluyó en un commit*
+
+= V.2.1.2 =
+    Estructuración general del proyecto
+Como proposito genreal, se estructuró el proyecto con dos subcarpetas con el objetivo de que se haga uso de archivos externos para poder leer, modificar y usar prompts optimizados con el uso de prompt engineering y la estructura de prompt propuesta por OpenAI.
+La estructra de subcarpetas y archivos propuesta en esta versión es:
+
+mercado200/
+|
++-- mercado200.py
++-- requeriments.txt
++-- LICENSE
++-- README.md
+|
++-- data/
+|   |
+|   +-- instrucciones.txt
+|   +-- plantilla00_OpenAI.txt
+|   ...
+|   +-- plantilla??_OpenAI.txt
+|
++-- utils/
+    |
+    +-- prompts.py
+
+NOTA: La estructura y funcionamiento serán ampliadas en la documentación anexa a las carpetas en  proximas versiones.
--- mercado200.py
+++ mercado200.py
@@ -1,6 +1,7 @@
 ###################################################
 #              Generador de leads                 #
 # V.2.0.0 //25 04 2025//                          #
+# V.2.0.2 //30 05 2025//                          #
 # Desplegado con streamlit                        #
 # Agentes impulsados con OpenAI                   #
 # Desarrollador: Sergio Emiliano López Bautista   #
@@ -15,6 +16,7 @@ import codecs
 import streamlit as st
 from dotenv import load_dotenv, find_dotenv
 from openai import OpenAI
+from utils.prompts import construir_prompt
 
 # --------------------------- Seteadores ----------------------------------------------
 st.set_page_config(page_title="Estudio de mercado", layout="wide")
@@ -28,10 +30,12 @@ st.title("Estudio de mercado")
 
 # --------------------------- Funciones -----------------------------------------------
 def agente(tema):
+    datos = {"tema": tema}
+    prompt = construir_prompt('data/plantilla01_OpenAI.txt', datos)
     try:
         agente = client.responses.create(
         model= "gpt-4.1",
-        input= f"Necesito un estudio de mercado acerca de {tema}"
+        input= prompt
         )
         return agente.output_text
     except Exception as e:
@@ -44,20 +48,26 @@ def maquina_de_escribir(respuesta):
         time.sleep(0.02)
 
 def instrucciones():
-    with codecs.open("instrucciones.txt", "r", encoding="utf-8") as f:
+    if not os.path.exists("data/instrucciones.txt"):
+        raise FileNotFoundError(f"El archivo {"data/instrucciones.txt"} no existe.")
+
+    with codecs.open("data/instrucciones.txt", "r", encoding="utf-8") as f:
         fi = f.read()
     file = fi.split('\n')
+
     for linea in file:
         st.markdown(linea)
 
 # -------------------------------- Interfaz (MAIN)-------------------------------------
 
-st.header("Dale, mano")
-tema = st.text_input("De qué tema buscas información")
+st.markdown("## ¡Bienvenido!")
+instrucciones()
+tema = st.text_input("¿De qué tema buscas información?")
 
 if st.button("investigar"):
     if tema:
         with st.spinner("investigando..."):
             st.markdown("### Vista previa de la información")
-            st.markdown(agente(tema))
-            #st.write_stream(maquina_de_escribir(agente(tema)))
+            st.write_stream(maquina_de_escribir(agente(tema)))
+    else:
+        st.markdown("Por favor, completa el campo.")
